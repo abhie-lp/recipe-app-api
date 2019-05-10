8c057851ef1696ec40d4d279c0104e820afab930
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-10 10:23:48 +0530

    Added wait_for_db management command

--- app/core/tests/tests_admin.py
+++ app/core/tests/tests_admin.py
@@ -33,7 +33,7 @@ class AdminSiteTests(TestCase):
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
-    
+
     def test_add_new_users_page(self):
         """Test that the create user page works"""
         url = reverse("admin:core_user_add")
