32580a4b8719384f9bbfc46523676354ec467719
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-15 18:54:35 +0530

    Added create ingredient feature

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -69,3 +69,25 @@ class PrivateIngredientAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]["name"], ingredient.name)
+
+    def test_create_ingredient_successfull(self):
+        """Test that creating a new ingredient is success"""
+
+        payload = {"name": "Cabbage"}
+
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload["name"]
+        ).exists()
+
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient failed"""
+        payload = {"name": ""}
+
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -25,7 +25,9 @@ class TagViewSet(viewsets.GenericViewSet,
         seriializer.save(user=self.request.user)
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
     authentication_classes = TokenAuthentication,
     permission_classes = IsAuthenticated,
     serializer_class = IngredientSerializer
@@ -34,3 +36,6 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
 
         return self.queryset.filter(user=self.request.user).order_by("-name")
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
