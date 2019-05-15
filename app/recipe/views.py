d228ae6a89d8d0269cec4ebd0ac73013f736a712
Abhishek Tiwari <abhie.lp@gmail.com>
2019-05-15 19:03:27 +0530

    Refactored views

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -6,36 +6,28 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in the database"""
-
-    serializer_class = TagSerializer
-    authentication_classes = TokenAuthentication,
-    permission_classes = IsAuthenticated,
-    queryset = Tag.objects.all()
-
-    def get_queryset(self):
-
-        return self.queryset.filter(user=self.request.user).order_by("-name")
-
-    def perform_create(self, seriializer):
-        """Create a new tag"""
-        seriializer.save(user=self.request.user)
-
-
-class IngredientViewSet(viewsets.GenericViewSet,
+class BaseRecipeViewSet(viewsets.GenericViewSet,
                         mixins.ListModelMixin,
                         mixins.CreateModelMixin):
+    """Base ViewSet for user owned recipe"""
+
     authentication_classes = TokenAuthentication,
     permission_classes = IsAuthenticated,
-    serializer_class = IngredientSerializer
-    queryset = Ingredient.objects.all()
 
     def get_queryset(self):
-
         return self.queryset.filter(user=self.request.user).order_by("-name")
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeViewSet):
+    """Manage tags in the database"""
+
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeViewSet):
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
